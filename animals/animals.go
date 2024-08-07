af838093af41fbab3446c46d2f2f4ac628abe727
Jose Vidal <jose_vidal@homedepot.com>
2024-08-07 12:13:53 -0600

    Changes in animal interface

--- animals/animals.go
+++ animals/animals.go
@@ -2,6 +2,23 @@ package animals
 
 import "github.com/macondiano100/GoLang/animals/internal/animals"
 
-func DoAnimal() {
+type Animal = int
+
+const (
+	DOG Animal = iota
+	CAT
+	PARROT
+)
+
+func DoAnimal(animal Animal) {
+	switch animal {
+	case DOG:
+		animals.Bark()
+	case CAT:
+		animals.Meow()
+	case PARROT:
+		println("Not supported")
+
+	}
 	animals.Meow()
 }
